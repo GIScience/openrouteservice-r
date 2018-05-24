88a6eed2806d904c0bd16a79048d784440b5da51
aoles <andrzej.oles@gmail.com>
2018-05-24 14:38:35 +0200

    Allow API key to be passed in environment variable (closes https://github.com/GIScience/openrouteservice-r/issues/28)

--- R/api_key.R
+++ R/api_key.R
@@ -1,30 +1,43 @@
 #' API key management
 #'
-#' Get/set the API key to openrouteservice.
+#' Get/set openrouteservice API key.
 #'
 #' To set the key provide it in the `key` argument. To retrieve the current
 #' value call the function with `key` unset.
 #'
-#' @return API Key value when called without `key`.
+#' Typically the key is saved in the system credential store. Once the key is
+#' defined, it persists in the keyring store of the operating system so it
+#' doesn't need to be set again in a new R session.
+#'
+#' Internally the function uses `\link[keyring]{key_set}` and
+#' `\link[keyring]{key_get}`. The use of keyring package can be bypassed by
+#' providing the key in the environment variable ORS_API_KEY. The value from the
+#' environment variable takes precedence over the value stored in the system
+#' credential store. The default environment variable name used to retrieve the
+#' openrouteservice api key can be overridden by specifying it in
+#' `options("openrouteservice.api_key_env")`.
+#'
 #' @param key API key value provided as a character scalar
-#' @importFrom keyring key_list key_get key_list key_set_with_value
 #' @inheritParams keyring::key_set
+#' @return API Key value when called without `key`.
 #' @template author
+#' @importFrom keyring key_list key_get key_list key_set_with_value
 #' @export
 ors_api_key <- function (key, service = 'openrouteservice', username = NULL, keyring = NULL) {
   ## get key
-  if ( missing(key) )
-    if ( runningCI() )
-      Sys.getenv("ORS_API_KEY")
+  if ( missing(key) ) {
+    api_key_env <- getOption("openrouteservice.api_key_env", "ORS_API_KEY")
+    api_key_val <- Sys.getenv(api_key_env)
+    ## api key set in environment variable takes precedence over keyring
+    if ( nchar(api_key_val) )
+      api_key_val
     else
-      if ( toString(username) %in% key_list(service, keyring)$username )
-        key_get(service, username, keyring)
-      else
-        stop(sprintf("API key not set.\n  Get your free key at %s\n  Use `ors_api_key('<your-api-key>')` to set it",
-                     signup_url()), call. = FALSE)
+      tryCatch(
+        key_get(service, username, keyring),
+        error = function(e)
+          stop(sprintf("API key not set.\n  Get your free key at %s\n  Use `ors_api_key('<your-api-key>')` to set it", signup_url()), call. = FALSE))
+  }
   ## set key
   else
     key_set_with_value(service, username, key, keyring)
 }
-
-runningCI <- function() isTRUE(as.logical(Sys.getenv("CI")))
--- tests/testthat/test-api_key.R
+++ tests/testthat/test-api_key.R
@@ -1,8 +1,41 @@
-context("api_key")
+context("API key management")
 
-test_that("running CI", {
-  Sys.setenv(CI="true")
-  expect_true(runningCI())
-  Sys.unsetenv("CI")
-  expect_false(runningCI())
+## record current state before starting any tests
+env <- Sys.getenv("ORS_API_KEY", NA);
+key <- tryCatch(keyring::key_get("openrouteservice"), error = function(e) NA)
+
+Sys.unsetenv("ORS_API_KEY")
+api_key_val <- "key_stored_in_keyring"
+
+test_that("Set key in keyring", {
+  expect_silent(ors_api_key(api_key_val))
 })
+
+test_that("Get key from keyring", {
+  expect_identical(ors_api_key(), api_key_val)
+})
+
+test_that("Environment variable takes precedance over keyring", {
+  ors_api_key <- Sys.getenv("ORS_API_KEY", NA)
+  on.exit(
+    if ( is.na(ors_api_key) )
+      Sys.unsetenv("ORS_API_KEY")
+    else
+      Sys.setenv(ORS_API_KEY = ors_api_key)
+  )
+  api_key_val <- "key_stored_in_env_var"
+  Sys.setenv(ORS_API_KEY = api_key_val)
+  expect_identical(ors_api_key(), api_key_val)
+})
+
+test_that("Use non-default environment variable for api key", {
+  options(openrouteservice.api_key_env = "MY_API_KEY")
+  api_key_val <- "my_api_key_value"
+  Sys.setenv(MY_API_KEY = api_key_val)
+  expect_identical(ors_api_key(), api_key_val)
+  Sys.unsetenv("MY_API_KEY")
+})
+
+## restore initial state
+if ( !is.na(env) ) Sys.setenv(ORS_API_KEY = env)
+if ( !is.na(key) ) keyring::key_set_with_value("openrouteservice", NULL, key)
