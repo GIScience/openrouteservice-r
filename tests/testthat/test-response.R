e61aa201d879eef4e4d85c24a70cb8c199e1dc06
aoles <andrzej.oles@gmail.com>
2018-05-29 17:00:20 +0200

    Add tests for GPX response

--- tests/testthat/test-response.R
+++ tests/testthat/test-response.R
@@ -9,3 +9,19 @@ mock_error <- mock_response(
 test_that("Error response", {
   expect_error(process_response(mock_error))
 })
+
+mock_gpx <- paste(readLines("mock_gpx.xml"), collapse = "\n")
+
+mock_xml <- mock_response(
+  headers = list("Content-Type" = "application/xml;charset=UTF-8"),
+  request = list(headers = list(Accept = "application/xml")),
+  content = charToRaw(mock_gpx)
+)
+
+test_that("Parsed GPX response", {
+  expect_equal(process_response(mock_xml, parse_output = TRUE), read_xml(mock_gpx))
+})
+
+test_that("Unparsed GPX response", {
+  expect_identical(process_response(mock_xml, parse_output = FALSE), mock_gpx)
+})
