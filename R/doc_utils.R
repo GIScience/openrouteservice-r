feec84e888e0e7e9b76bc32f56c1d8b0c6e59198
Patrick Hausmann <patrick.hausmann@covimo.de>
2019-10-08 12:52:50 +0200

    Fix link to sign-up page

--- R/doc_utils.R
+++ R/doc_utils.R
@@ -5,7 +5,7 @@ doc_url <- function(service) {
 doc_link <- function(service, label=service) sprintf("[%s](%s)", label, doc_url(service))
 
 signup_url <- function (label) {
-  url <- "https://openrouteservice.org/sign-up"
+  url <- "https://openrouteservice.org/dev/#/sign-up"
   if ( missing(label) )
     url
   else
